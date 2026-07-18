d0cf9a59fed9560cee18c4734acc157c9861fb2d
JohnVitor-DEV <johnvitoralves39@gmail.com>
2026-07-18 09:54:36 -0300

    feat: add routing and route parameters to ProductsController

--- Loja.Api/Controllers/ProdutosController.cs
+++ Loja.Api/Controllers/ProdutosController.cs
@@ -8,8 +8,17 @@ namespace Loja.Api.Controllers;
 public class ProdutosController : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get()
+    public IActionResult Get(decimal minPrice, decimal maxPrice)
     {
+        if (minPrice > 0 || maxPrice > 0)
+            return Ok($"MinPrice: {minPrice} \nMaxPrice: {maxPrice}");
+
         return Ok(Produto.Produtos);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok(id);
     }
 }
