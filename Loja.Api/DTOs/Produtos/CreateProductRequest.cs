f853623204dc137da5056a78af5128b1c98863d8
JohnVitor-DEV <johnvitoralves39@gmail.com>
2026-07-19 23:54:57 -0300

    feat: add request validation using data annotations

--- Loja.Api/DTOs/Produtos/CreateProductRequest.cs
+++ Loja.Api/DTOs/Produtos/CreateProductRequest.cs
@@ -1,4 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 public record CreateProductRequest(
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
     string Name,
+
+    [Range(0.01, 100000)]
     decimal Price
 );
